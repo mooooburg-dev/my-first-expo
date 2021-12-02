9d8220c572fff69148c210130335350c0a3e942b
mooooburg-dev <jeongmupark@gmail.com>
2021-12-02 23:44:54 +0900

    App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log('React Navite App')
+  return (
+    <View style={styles.container}>
+      <Text>Hi, Jeongmu Park!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
